dc079e7a1a82ba02bd944d4ff908da4d349e7c07
SumjyoLama <sumjyohyolmo443@gmail.com>
2023-05-29 11:30:48 -1200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+ const express = require("express");
+ const dotenv = require("dotenv");
+ const {chats} = require("./data/data.js");
+ const connectDB = require("./config/db.js");
+ const userRoutes = require("./routes/userRoutes");
+ const chatRoutes = require("./routes/chatRoutes");
+ const {notFound,errorHandler} = require("./middleware/errorMiddleware.js")
+ 
+
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());//to accept JSON data
+
+
+app.get("/", (req, res)=> {
+    res.send("API is Running Successfully");
+});
+
+app.use('/api/user', userRoutes)
+app.use('/api/chat',chatRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000,console.log(`Server Started on PORT ${PORT}`)); 
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,58 @@
+
+import React from 'react'
+import { Container,
+         Box,
+        Text,
+        Tab,
+        TabList,
+        TabPanel,
+        TabPanels,
+        Tabs,
+       } from '@chakra-ui/react'
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
+
+const Homepage = () => {
+  return (
+    <Container maxW = "xl" centerContent>
+      <Box
+        d = 'flex'
+        justifyContent="center"
+        p = {3}
+        bg = {"white"}
+        w= "100%"
+        m = "40px 0 15px"
+        borderRadius="lg"
+        borderWidth="1px"
+        >
+
+
+        <Text fontSize={"4xl" }fontFamily={"work sans"} color = "black" textAlign={"center"}>Guff-Gaff</Text>
+      </Box>
+      <Box 
+      bg = "white" 
+      w = "100%"
+      p = {4}
+      borderRadius={ "lg"}
+      color = "black"
+      borderWidth={"1px"} >
+        <Tabs variant='soft-rounded'>
+  <TabList mb = "1em">
+    <Tab width = "50%">Login</Tab>
+    <Tab width = "50%">Signup</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <Login />
+    </TabPanel>
+    <TabPanel>
+      <Signup />
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </Box>
+    </Container>
+  )
+}
+
+export default Homepage
